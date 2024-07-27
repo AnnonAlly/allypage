afec236661053c349794cc3579e9760e32bf89a6
mil4ne <josemilane1322@gmail.com>
2024-07-26 20:17:45 -0400

    v1.0

---
+++ _astro/_slug_.78391675.ed-UwhIr.js
@@ -0,0 +1,1 @@
+const _="_h1_15kja_29",o="_h2_15kja_30",t="_h3_15kja_31",s="_h4_15kja_32",r="_h5_15kja_33",n="_h6_15kja_34",a="_p_15kja_35",e="_emphasize_15kja_49",i={h1:_,h2:o,h3:t,h4:s,h5:r,h6:n,p:a,"dark-mode":"_dark-mode_15kja_40",emphasize:e};export{i as s};
